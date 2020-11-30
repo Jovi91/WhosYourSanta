bc4dd2d9121e30a6945d69ccc89d5bce12ab7f67
Jovi <jowita.daszkiewicz@gmail.com>
2020-11-30 17:08:12 +0100

    initialize Santa object to match MainViev branch

--- WhosYourSanta/Models/Lottery.cs
+++ WhosYourSanta/Models/Lottery.cs
@@ -12,6 +12,6 @@ namespace WhosYourSanta.Models
         [Required]
         public string Name { get; set; }
         public IdentityUser Admin { get; set; }
-        public List<Santa> Santas { get; set; }
+        public List<Santa> Santas { get; set; } = new List<Santa>();
     }
 }
