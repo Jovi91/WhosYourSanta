64eec187957c6b2e7a8fe67d862298343d8bf0b3
Jovi <jowita.daszkiewicz@gmail.com>
2021-01-07 20:21:38 +0100

    Main View displays well on IE browser
    *background, slides animations and resize browser window works well on IE
    *fixed display after resizing browser window on chrome

--- WhosYourSanta/wwwroot/js/Main.js
+++ WhosYourSanta/wwwroot/js/Main.js
@@ -24,27 +24,39 @@ function changeProperties() {
 
     if (numberOfLotteries == 0)
         document.getElementsByClassName('allMyLotteries').width = 0;
+    else {
+        
 
-    if (screen.width < 600 && numberOfLotteries > 0) {
-        document.getElementsByClassName('allMyLotteries').width = "100%";
-        var buttonHeight = 100 / (numberOfLotteries + 1);
-        var lotteriesSectionHeight = 100 - buttonHeight;
+        if ($(window).width() < 600 && numberOfLotteries > 0) {
+            document.getElementsByClassName('allMyLotteries').width = "100%";
+            var buttonHeight = 100 / (numberOfLotteries + 1);
+            var lotteriesSectionHeight = 100 - buttonHeight;
+        }
+        else {
+            document.getElementsByClassName('allMyLotteries').width = "50%";
+            var buttonHeight = 100;
+            var lotteriesSectionHeight = 100;
+        }
         var buttonHeightPercent = buttonHeight.toString() + "%";
         var lotteriesSectionHeightPercent = lotteriesSectionHeight.toString() + "%";
         document.getElementById("addLotterybtnId").style.height = buttonHeightPercent;
         document.getElementById("allMyLotteriesId").style.height = lotteriesSectionHeightPercent;
-    }
-    else if (screen.width > 600 && numberOfLotteries > 0) {
-        document.getElementsByClassName('allMyLotteries').width = "50%";
-
-  
 
     }
-
-       
-
 
 
 }
 window.onresize = changeProperties;
 window.onload = changeProperties;
+
+
+//var resizeTimer;
+
+//$(window).on('resize', function (e) {
+//    clearTimeout(resizeTimer);
+//    resizeTimer = setTimeout(function () {
+
+
+//    }, 250);
+
+//});
