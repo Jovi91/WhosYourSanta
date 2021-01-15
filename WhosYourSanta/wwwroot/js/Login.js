f77f8253c99e442c07d79238e912e90446e81b23
Jovi <jowita.daszkiewicz@gmail.com>
2021-01-15 20:00:07 +0100

    Add temporary Login option: Login as a guest

---
+++ WhosYourSanta/wwwroot/js/Login.js
@@ -0,0 +1,11 @@
+﻿function LoginAsGuest() {
+    if (document.getElementById('validateIfGuest').checked) {
+        document.getElementById('userEmailInput').value = "guest@wp.pl";
+        document.getElementById('userPasswordInput').value = "Iamguest_1";
+    }
+    else
+    {
+        document.getElementById('userEmailInput').value = "";
+        document.getElementById('userPasswordInput').value = "";
+    }
+}
